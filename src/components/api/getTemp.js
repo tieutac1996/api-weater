22115c801bad07c5262817945c0926ae4f3b2042
tieutac1996 <vietkaka1996@gmail.com>
2020-05-16 21:53:35 +0700

    Update getTemp.js

--- src/components/api/getTemp.js
+++ src/components/api/getTemp.js
@@ -1,5 +1,5 @@
 const URL =
-  "http://api.openweathermap.org/data/2.5/weather?appid=176eb9d535508235e116000ce82b167b&q=";
+  "https://api.openweathermap.org/data/2.5/weather?appid=176eb9d535508235e116000ce82b167b&q=";
 
 export default function getTemp(cityName) {
   return fetch(URL + cityName)
